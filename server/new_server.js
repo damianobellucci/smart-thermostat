df516d2f6a76a3fbc2485dd8214aaa5c999a9c2e
Damiano <belluccidamiano@gmail.com>
2021-12-18 15:44:29 +0100

    update

--- server/new_server.js
+++ server/new_server.js
@@ -1,3 +1,5 @@
+const URL_HARDWARE = "http://192.168.1.30:80"
+
 /***********CONFIGURE CLIENT FOR INTERACTION WITH ESP32 */
 const configHeaders = {
     headers: {
@@ -35,6 +37,21 @@ const validate_setparameters = ajv.compile(
         minProperties: 1
     }
 )
+
+const validate_currentstate = ajv.compile(
+    {
+        type: "object",
+        properties: {
+            "temperature": { type: "string" },
+            "threshold": { type: "string" },
+            "status": { type: "string" },
+            "knights": { type: "string" },
+        },
+        additionalProperties: false,
+        minProperties: 1,
+        maxProperties: 4
+    }
+)
 /****************************/
 
 app.post('/setparameters', function (req, res) {
@@ -46,14 +63,64 @@ app.post('/setparameters', function (req, res) {
         for (let [key, value] of Object.entries(req.body)) {
             params.append(key, value)
         }
-        axios.post("http://192.168.1.30:80/setparameters", params, configHeaders)
+        axios(
+            {
+                method: 'post',
+                url: URL_HARDWARE + "/setparameters",
+                data: params,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                timeout: 10000 //timeout in milliseconds
+            }
+        )
             .then((result) => {
-                res.status(204).send()
+                res.status(204)
             })
             .catch((err) => {
                 res.status(502).send({ error: "The parameters have not been set. Impossible to reach..." })
             })
     }
+});
+
+function parseDataCurrentState(data) {
+    let obj = {}
+    for (const element of data.split(";").slice(0, -1)) {
+        let el = element.split(":")
+        if (el[0] == "knights") {
+            let list = el[1].split(",")
+            let obj_knights = {}
+            for (let i = 0; i < list.length; i++) {
+                obj_knights[i] = Number(list[i])
+            }
+            obj[el[0]] = obj_knights
+        }
+        else obj[el[0]] = Number(el[1])
+    }
+    return obj
+}
+
+app.get('/currentstate', function (req, res) {
+    if (!validate_currentstate(req.query)) {
+        res.status(400).send({ error: validate_currentstate.errors })
+    }
+    else {
+        axios(
+            {
+                method: "get",
+                url: URL_HARDWARE + "/currentstate",
+                params: req.query,
+                timeout: 10000
+            }
+        )
+            .then((result) => {
+                res.status(200).send(parseDataCurrentState(result.data))
+            })
+            .catch((err) => {
+                res.status(502).send({ error: err.code })
+            })
+    }
+
 });
 
 
